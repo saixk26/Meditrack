809127aa3ad97fd2476d5bd51a1770a87660a7a6
saixk26 <bwwsaikishori@gmail.com>
2026-09-14 14:36:29 +0530

    Update ALLOWED_HOSTS and switch database to SQLite

--- meditrack/meditrack/settings.py
+++ meditrack/meditrack/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = ['*']
 
 # -------------------------------------------------------------------------
 # APPLICATION DEFINITION
@@ -84,16 +84,8 @@ ASGI_APPLICATION = 'meditrack.asgi.application'
 #   set DB_PORT=3306
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME', 'meditrack_db'),
-        'USER': os.environ.get('DB_USER', 'root'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'root'),
-        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
-        'PORT': os.environ.get('DB_PORT', '3306'),
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
